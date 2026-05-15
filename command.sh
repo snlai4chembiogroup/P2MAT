2a9eb39a76db5436ea6289520872d6b4c451c3d0
“snlai4chembiogroup” <snlai.chembiogroup@gmail.com>
2026-05-15 17:22:11 -0500

    reshuffle files

--- command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CONDA_BASE_DIR=/opt/miniconda3
-source "$CONDA_BASE_DIR/etc/profile.d/conda.sh"
-
-conda activate qsai
-
-python p2mat.py
--- qsai_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda remove --name qsai --all --yes
-conda env create -f qsai.yml -n qsai
-
-conda activate qsai
-python -m ipykernel install --user --name qsai --display-name "Python 3.12 (QSAI)"
